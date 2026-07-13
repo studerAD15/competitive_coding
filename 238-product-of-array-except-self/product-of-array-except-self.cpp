ec42caf070bdfa272a07a7f5ee99d396b1984eaf
studerAD15 <adichhikaraa@gmail.com>
2026-07-13 12:22:16 +0530

    Time: 4 ms (21.25%) | Memory: 40.1 MB (81.40%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,34 +2,34 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         vector<int> result(nums.size(),1);
-        vector<int> post(nums.size(),1);
-        vector<int> pre(nums.size(),1);
-        for(int i=1;i<nums.size();i++)
-        {
-            pre[i]=nums[i-1]*pre[i-1];
-        }
-        for(int i=nums.size()-2;i>=0;i--)
-        {
-            post[i]=nums[i+1]*post[i+1];
-        }
-        for(int i=0;i<nums.size();i++)
-        {
-            result[i]=pre[i]*post[i];
-        }
-        return result;
-
-        // int pre=1;
+        // vector<int> post(nums.size(),1);
+        // vector<int> pre(nums.size(),1);
+        // for(int i=1;i<nums.size();i++)
+        // {
+        //     pre[i]=nums[i-1]*pre[i-1];
+        // }
+        // for(int i=nums.size()-2;i>=0;i--)
+        // {
+        //     post[i]=nums[i+1]*post[i+1];
+        // }
         // for(int i=0;i<nums.size();i++)
         // {
-        //     result[i]=pre;
-        //     pre=nums[i]*pre;
+        //     result[i]=pre[i]*post[i];
         // }
-        // int post=1;
-        // for(int i=nums.size()-1;i>=0;i--)
-        // {
-        //     result[i]*=post;
-        //     post=nums[i]*post;
-        // }
+        // return result;
+
+        int pre=1;
+        for(int i=0;i<nums.size();i++)
+        {
+            result[i]=pre;
+            pre=nums[i]*pre;
+        }
+        int post=1;
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+            result[i]*=post;
+            post=nums[i]*post;
+        }
         return result;
     }
 };
