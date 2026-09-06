67ad833576e951d84abdc9d3bf88779d7afc3e53
studerAD15 <adichhikaraa@gmail.com>
2026-09-06 12:43:28 +0530

    Time: 2 ms (21.88%) | Memory: 20.8 MB (56.15%) - LeetSync

---
+++ 4410-count-integers-appearing-in-a-single-block/count-integers-appearing-in-a-single-block.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int countSpecialIntegers(vector<int>& nums) {
+        int n=nums.size();
+        int left=0;
+        int right=1;
+        unordered_map<int,int> mp;
+        int ans=0;
+        for(int i:nums)
+        {
+            mp[i]++;
+        }
+        while(left <n)
+        {
+            if(mp[nums[left]]==1)
+            {
+                ans++;
+                left++;
+                continue;
+            }
+            right=left+1;
+            while(right<n && nums[left]==nums[right])
+            {
+                right++;
+            }
+            int win=right-left;
+            if(mp[nums[left]]==win)
+            {
+                ans++;
+            }
+            left=right;
+        }
+        return ans;
+    }
+};
