6b4f01947e14f45a883a0e5cc1832330e8452176
studerAD15 <adichhikaraa@gmail.com>
2026-09-12 10:26:05 +0530

    Time: 0 ms (100.00%) | Memory: 34.3 MB (87.01%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -20,37 +20,36 @@ public:
         return min(root->left);
     }
     TreeNode* deleteNode(TreeNode* root, int key) {
-       if(root==nullptr)
-       {
-        return nullptr;
-       }
-       if(key<root->val)
-       {
-        root->left=deleteNode(root->left,key);
-       }
-       else if(key>root->val)
-       {
-        root->right=deleteNode(root->right,key);
-       }
-       else
-       {
-        if(root->left==nullptr)
+        if(root==nullptr)
         {
-            TreeNode* temp=root->right;
-            delete root;
-            return temp;
+            return nullptr;
         }
-        if(root->right==nullptr)
+        if(root->val>key)
         {
-            TreeNode* temp=root->left;
-            delete root;
-            return temp;
+            root->left=deleteNode(root->left,key);
         }
-        TreeNode* succesor=min(root->right);
-        root->val=succesor->val;
-        root->right=deleteNode(root->right,succesor->val);
-
-       }
-       return root;
+        else if(root->val<key)
+        {
+            root->right=deleteNode(root->right,key);
+        }
+        else
+        {
+            if(root->right==nullptr)
+            {
+                TreeNode* temp=root->left;
+                delete root;
+                return temp;
+            }
+            if(root->left==nullptr)
+            {
+                TreeNode* temp=root->right;
+                delete root;
+                return temp;
+            }
+            TreeNode* succesor=min(root->right);
+            root->val=succesor->val;
+            root->right=deleteNode(root->right,succesor->val);
+        }
+        return root;
     }
 };
