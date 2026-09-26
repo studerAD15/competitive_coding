d806fde845920c14b1999b6a078ab99e4eb3cb1a
studerAD15 <adichhikaraa@gmail.com>
2026-09-26 12:08:46 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (37.58%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -6,13 +6,13 @@ public:
         while(left<=right)
         {
             int mid=left+(right-left)/2;
-            if(nums[mid]==target)
+            if(target==nums[mid])
             {
                 return mid;
             }
             if(nums[left]<=nums[mid])
             {
-                if(target<=nums[mid] && target>=nums[left])
+                if(nums[mid]>target && target>=nums[left])
                 {
                     right=mid-1;
                 }
@@ -21,16 +21,17 @@ public:
                     left=mid+1;
                 }
             }
-            else{
-            if(target<=nums[right] && target>=nums[mid])
+            else
             {
-                left=mid+1;
+                if(nums[mid]<target && target<=nums[right])
+                {
+                    left=mid+1;
+                }
+                else
+                {
+                    right=mid-1;
+                }
             }
-            else{
-                right=mid-1;
-            }
-            }
-
         }
         return -1;
     }
